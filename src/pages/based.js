021fbb987a9881820dd640c14750651525a646ca
shoopdasoup <shoopdasoup@yahoo.com>
2020-08-12 01:55:55 -0700

    based

---
+++ src/pages/based.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Based = () => (
+  <Layout>
+    <SEO title="BIG N BASED" />
+    <h1>My appendage is apprehensive of my atrocious anus.</h1>
+    <p>CHODE</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default Based
